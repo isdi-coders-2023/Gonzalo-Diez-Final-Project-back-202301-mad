a86f04511e3041e873d3191ef2d8e31983802de3
gonzu <gonzalodiezdavila@gmail.com>
2023-03-14 18:09:41 +0100

    🐛 Fix JS importation

--- src/repositories/Users/users.mongo.repo.ts
+++ src/repositories/Users/users.mongo.repo.ts
@@ -1,4 +1,4 @@
-import { User } from '../../entities/user';
+import { User } from '../../entities/user.js';
 import { URepo } from './users.repo.interface';
 import createDebug from 'debug';
 import { UserModel } from './user.mongo.model';
