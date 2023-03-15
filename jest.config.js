e10725bd70acd9ad840911bc674a7da4b62b66a7
gonzu <gonzalodiezdavila@gmail.com>
2023-03-15 13:26:59 +0100

    🚑 Fix SonarCloud router coverage bug

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,20 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['dist', 'user.mongo.model.ts'],
+  testPathIgnorePatterns: [
+    'dist',
+    'user.mongo.model.ts',
+    'addictions.mongo.model.ts',
+    'users.router.ts',
+  ],
   resolver: 'jest-ts-webcompat-resolver',
   collectCoverageFrom: ['src/**/*.ts'],
   coveragePathIgnorePatterns: [
     'index.ts',
     'app.ts',
-    'routers',
+    'users.router.ts',
     'entities',
     'user.mongo.model.ts',
+    'addictions.mongo.model.ts',
   ],
 };
