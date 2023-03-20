a48ce4e93be440dd736ad12eca0186dd706d1ddf
gonzu <gonzalodiezdavila@gmail.com>
2023-03-20 16:01:18 +0100

    🌱 Add conditions controller & testing

---
+++ src/controllers/Addictions/addictions.controller.test.ts
@@ -0,0 +1,104 @@
+import { Request, Response } from 'express';
+import { AddictionsController } from './addictions.controller';
+import { AddictionsMongoRepo } from '../../repositories/Addictions/addictions.mongo.repo';
+import { UsersMongoRepo } from '../../repositories/Users/users.mongo.repo';
+
+describe('Given Addictions controller', () => {
+  const repo: AddictionsMongoRepo = {
+    read: jest.fn(),
+    readId: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+
+  const userRepo = {} as UsersMongoRepo;
+
+  const req = {
+    body: {},
+    params: { id: '' },
+  } as unknown as Request;
+
+  const resp = {
+    json: jest.fn(),
+  } as unknown as Response;
+
+  const next = jest.fn();
+
+  const controller = new AddictionsController(repo, userRepo);
+
+  describe('When toLoad method is called', () => {
+    test('Then, it should return the data if there isnt any problem', async () => {
+      await controller.toLoad(req, resp, next);
+      expect(repo.read).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+    test('Then, if there is any error it should call next', async () => {
+      (repo.read as jest.Mock).mockRejectedValue(new Error());
+      await controller.toLoad(req, resp, next);
+      expect(repo.read).toHaveBeenCalled();
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When toLoadID method is called', () => {
+    test('Then it should call the repo method', async () => {
+      await controller.toLoadID(req, resp, next);
+      expect(repo.readId).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+    test('Then, if there is any error it should call next', async () => {
+      (repo.readId as jest.Mock).mockResolvedValue(new Error());
+      await controller.toLoadID(req, resp, next);
+      expect(repo.readId);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When delete method is called', () => {
+    test('Then, it should work if there is no errors', async () => {
+      const req = {
+        params: {
+          id: '1',
+        },
+      } as unknown as Request;
+
+      await controller.toDelete(req, resp, next);
+      expect(repo.delete).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+    test('Then, if there is any error it should call next function', async () => {
+      const req = {
+        params: {
+          id: undefined,
+        },
+      } as unknown as Request;
+
+      await controller.toDelete(req, resp, next);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('Given the toEdit method', () => {
+    const req = {
+      body: {
+        id: '1',
+      },
+      params: {
+        id: '1',
+      },
+    } as unknown as Request;
+    test('Then, if everythings its Ok it should call the repo method', async () => {
+      await controller.toEdit(req, resp, next);
+      expect(repo.update).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+    test('Then, if there is any error it should call next', async () => {
+      const req = {
+        params: {
+          id: undefined,
+        },
+      } as unknown as Request;
+      (repo.update as jest.Mock).mockResolvedValue(new Error());
+      await controller.toEdit(req, resp, next);
+      expect(repo.update).toHaveBeenCalled();
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/controllers/Addictions/addictions.controller.ts
@@ -0,0 +1,75 @@
+import { ARepo } from '../../repositories/Addictions/addictions.repo.interface';
+import createDebug from 'debug';
+import { NextFunction, Request, Response } from 'express';
+import { Addiction, UserAddiction } from '../../entities/addiction';
+import { HTTPError } from '../../errors/error';
+import { RequestPlus } from '../../interceptors/auth.interceptor';
+import { URepo } from '../../repositories/Users/users.repo.interface';
+import { User } from '../../entities/user';
+
+const debug = createDebug('MH:addictions-controller');
+
+export class AddictionsController {
+  constructor(
+    public AddictionsRepo: ARepo<UserAddiction>,
+    public userRepo: URepo<User>
+  ) {
+    this.AddictionsRepo = AddictionsRepo;
+    this.userRepo = userRepo;
+  }
+
+  async toLoad(req: Request, resp: Response, next: NextFunction) {
+    try {
+      const data = await this.AddictionsRepo.read();
+      resp.json({
+        result: data,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async toLoadID(req: Request, resp: Response, next: NextFunction) {
+    try {
+      const data = await this.AddictionsRepo.readId(req.params.id);
+      if (data) {
+        resp.json({
+          result: data,
+        });
+      } else {
+        resp.status(404).json({ message: 'ID not found' });
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async toDelete(req: Request, resp: Response, next: NextFunction) {
+    try {
+      if (!req.params.id)
+        throw new HTTPError(404, 'Not found', 'Params ID was not found');
+
+      await this.AddictionsRepo.delete(req.params.id);
+
+      resp.json({
+        result: [],
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async toEdit(req: Request, resp: Response, next: NextFunction) {
+    try {
+      if (!req.params.id)
+        throw new HTTPError(404, 'Not found', 'Params ID was not found');
+      req.body.id = req.params.id;
+      const data = await this.AddictionsRepo.update(req.body);
+      resp.json({
+        results: [data],
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
