4bd6fb1748c2d594736aae171cf961d7b2de98bb
gonzu <gonzalodiezdavila@gmail.com>
2023-03-25 02:14:24 +0100

    ✅ Update users controller

--- src/controllers/users.controller.test.ts
+++ src/controllers/users.controller.test.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 import { User } from '../entities/user';
 import { URepo } from '../repositories/Users/users.repo.interface';
 import { Auth } from '../helpers/auth';
+import { RequestPlus } from '../interceptors/auth.interceptor';
 
 jest.mock('../helpers/auth');
 
@@ -12,6 +13,8 @@ describe('Given the user controller', () => {
   const repoMock = {
     create: jest.fn(),
     search: jest.fn(),
+    addAddiction: jest.fn(),
+    deleteAddiction: jest.fn(),
   } as unknown as URepo<User>;
 
   const controller = new UserController(repoMock);
@@ -75,7 +78,7 @@ describe('Given the user controller', () => {
       await controller.login(req, resp, next);
       expect(next).toHaveBeenCalled();
     });
-    test('Then, if there is no password, it should return the next funtion', async () => {
+    test('Then, if there is no password, it should return the next function', async () => {
       const req = {
         body: {
           email: 'ThisIsAtest@test.com',
@@ -107,6 +110,192 @@ describe('Given the user controller', () => {
       (repoMock.search as jest.Mock).mockReturnValue(['test']);
       Auth.toUnHash = jest.fn().mockResolvedValue(false);
       await controller.login(req, resp, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When we use the addAddiction method', () => {
+    test('Then, it should return user addiction data when successfully added', async () => {
+      const userId = '1';
+      const addiction = 'Alcohol';
+      const timeConsuming = 2;
+      const cause = 'Social pressure';
+
+      const req = {
+        info: { id: userId },
+        body: { addiction, timeConsuming, cause },
+      } as RequestPlus;
+
+      const userAddiction = { id: 1, userId, addiction, timeConsuming, cause };
+      const addAddictionMock = jest.fn().mockResolvedValue(userAddiction);
+      const updateMock = jest.fn().mockResolvedValue(userAddiction);
+
+      repoMock.addAddiction = addAddictionMock;
+      repoMock.update = updateMock;
+
+      const resp = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as unknown as Response;
+
+      await controller.addAddiction(req, resp, next);
+
+      expect(addAddictionMock).toHaveBeenCalledWith(
+        userId,
+        addiction,
+        timeConsuming,
+        cause
+      );
+      expect(updateMock).toHaveBeenCalledWith(userAddiction);
+      expect(resp.status).toHaveBeenCalledWith(202);
+      expect(resp.json).toHaveBeenCalledWith({ results: [userAddiction] });
+    });
+
+    test('Then, if there is any error it should call Next function', async () => {
+      const userId = '1';
+      const addiction = 'Alcohol';
+      const timeConsuming = 2;
+      const cause = 2;
+
+      const req = {
+        params: { userId },
+        body: { addiction, timeConsuming, cause },
+      } as unknown as Request;
+
+      await controller.addAddiction(req, resp, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When we use the deleteAddictionController method', () => {
+    test('Then, it should call the repo method, and should return the resp', async () => {
+      const userMock = {
+        id: '1',
+        Addictions: ['1'],
+      };
+
+      const req = {
+        params: {
+          userId: '1',
+          index: 0,
+        },
+        info: { id: '1' },
+      } as unknown as RequestPlus;
+
+      repoMock.deleteAddiction = jest.fn().mockResolvedValue(userMock);
+
+      await controller.deleteAddiction(req, resp, next);
+      expect(repoMock.update).toHaveBeenCalled();
+      expect(repoMock.deleteAddiction).toHaveBeenCalledWith('1', 0);
+      expect(resp.json).toHaveBeenCalled();
+    });
+    test('Then, if there is any error, it should call next', async () => {
+      const userMock = {
+        id: '1',
+        Addictions: [],
+      };
+
+      const req = {
+        params: {
+          userId: userMock,
+          index: 4,
+        },
+      } as unknown as Request;
+
+      await controller.deleteAddiction(req, resp, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When we use the addCondition method', () => {
+    test('Then, it should return user condition data when successfully added', async () => {
+      const userId = '1';
+      const condition = 'depression';
+      const timeConsuming = 2;
+      const cause = 'Social pressure';
+
+      const req = {
+        info: { id: userId },
+        body: { condition, timeConsuming, cause },
+      } as RequestPlus;
+
+      const userCondition = { id: 1, userId, condition, timeConsuming, cause };
+      const addConditionMock = jest.fn().mockResolvedValue(userCondition);
+      const updateMock = jest.fn().mockResolvedValue(userCondition);
+
+      repoMock.addCondition = addConditionMock;
+      repoMock.update = updateMock;
+
+      const resp = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as unknown as Response;
+
+      await controller.addCondition(req, resp, next);
+
+      expect(addConditionMock).toHaveBeenCalledWith(
+        userId,
+        condition,
+        timeConsuming,
+        cause
+      );
+      expect(updateMock).toHaveBeenCalledWith(userCondition);
+      expect(resp.status).toHaveBeenCalledWith(202);
+      expect(resp.json).toHaveBeenCalledWith({ results: [userCondition] });
+    });
+
+    test('Then, if there is any error it should call Next function', async () => {
+      const userId = '1';
+      const condition = 'depression';
+      const timeConsuming = 2;
+      const cause = 2;
+
+      const req = {
+        params: { userId },
+        body: { condition, timeConsuming, cause },
+      } as unknown as Request;
+
+      await controller.addCondition(req, resp, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When we use the deleteCondition method', () => {
+    test('Then, it should call the repo method, and should return the resp', async () => {
+      const userMock = {
+        id: '1',
+        conditions: ['1'],
+      };
+
+      const req = {
+        params: {
+          userId: '1',
+          index: 0,
+        },
+        info: { id: '1' },
+      } as unknown as RequestPlus;
+
+      repoMock.deleteCondition = jest.fn().mockResolvedValue(userMock);
+
+      await controller.deleteCondition(req, resp, next);
+      expect(repoMock.update).toHaveBeenCalled();
+      expect(repoMock.deleteCondition).toHaveBeenCalledWith('1', 0);
+      expect(resp.json).toHaveBeenCalled();
+    });
+    test('Then, if there is any error, it should call next', async () => {
+      const userMock = {
+        id: '1',
+        conditions: [],
+      };
+
+      const req = {
+        params: {
+          userId: userMock,
+          index: 4,
+        },
+      } as unknown as Request;
+
+      await controller.deleteCondition(req, resp, next);
 
       expect(next).toHaveBeenCalled();
     });
